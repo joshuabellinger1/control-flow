bc04b9e4caf371be9cca67436444f449dcb764cd
Joshua Bellinger <Josh@Jeanettes-MacBook-Air.local>
2024-01-17 18:08:14 -0600

    Exercise 2 from Control Flow section

---
+++ Exercises/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Exercises;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var sum = 0;
+
+        while (true)
+        {
+            Console.WriteLine("Please enter a number, or type \"OK\" to EXIT");
+
+            var input = Console.ReadLine();
+
+            if (input.ToUpper() == "OK")
+            {
+                break;
+            }
+            else
+            {
+                sum += int.Parse(input);
+                Console.WriteLine(sum);
+            }
+        }
+        Console.WriteLine("You have been exited from this program. Here is your sum: " + sum);
+    }
+}
